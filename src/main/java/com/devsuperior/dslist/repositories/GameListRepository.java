52f76914ff29b27a4843e3582c34534b5f10ab7d
luan.nascimento <luan.nascimento@segsat.com>
2025-01-21 14:02:21 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -1,9 +1,9 @@
 package com.devsuperior.dslist.repositories;
 
-import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameListRepository extends JpaRepository<GameList, Long> {
 }
